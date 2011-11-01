97dfb28366669afa10fd922b6e075ce4a9a1250f
alta189 <stephendwilliams7@gmail.com>
2011-11-01 12:40:18 -0400

    Major refactor of User

--- src/main/java/com/alta189/chavabot/User.java
+++ src/main/java/com/alta189/chavabot/User.java
@@ -1,19 +1,49 @@
 package com.alta189.chavabot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: alta189
  */
-public interface User {
+public abstract class User {
+	protected String nick;
+	protected String fullName;
+	protected Type type;
+	protected List<Channel> channels = new ArrayList<Channel>();
 
-	public String getNick();
 
-	public String getPrefix();
+	public String getNick() {
+		return nick;
+	}
 
-	public boolean isOp(String channel);
+	public String getPrefix() {
+		return null;
+	}
 
-	public boolean hasOp(String channel);
+	public boolean hasOp(String channel) {
+		return false;
+	}
 
-	public String getHost();
+	public String getHost() {
+		return null;
+	}
 
-	public String getFullName();
+	public String getFullName() {
+		return fullName;
+	}
+
+	public boolean inChannel(String channel) {
+		for (Channel chan : channels) {
+			if (chan.toString().equalsIgnoreCase(channel)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public enum Type {
+		NORMAL_USER,
+		IRC_OPER;
+	}
 }
